36267cb091baff5f8932a8cd8944544770ec23cd
Nkseervi <nkseervi@gmail.com>
2023-11-01 17:18:00 +0530

    Add User Controller

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreateDate { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
