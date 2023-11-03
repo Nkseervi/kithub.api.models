879b3f91e6c148c9c5f930970d27e1e02f1685ec
Nkseervi <nkseervi@gmail.com>
2023-11-03 18:58:27 +0530

    authentication token management

---
+++ Dtos/LoggedInUserDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+	public class LoggedInUserDto
+	{
+		public string Token { get; set; }
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public DateTime CreateDate { get; set; }
+		public void ResetUserModel()
+		{
+			Token = string.Empty;
+			Id = string.Empty;
+			FirstName = string.Empty;
+			LastName = string.Empty;
+			EmailAddress = string.Empty;
+			CreateDate = DateTime.MinValue;
+		}
+	}
+}
