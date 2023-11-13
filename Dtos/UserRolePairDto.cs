07ffe5614fbf973c74e331b73176f9bbd5f11b9c
Nkseervi <nkseervi@gmail.com>
2023-11-13 17:10:20 +0530

    Added roles operations to User Controller

---
+++ Dtos/UserRolePairDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class UserRolePairDto
+    {
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
