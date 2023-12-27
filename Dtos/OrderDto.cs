7e86519d68fb4745edf4476bb9453ba26003b943
Nkseervi <nkseervi@gmail.com>
2023-12-27 10:48:10 +0530

    Order dtos

---
+++ Dtos/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public int Amount { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public List<OrderItemDto> OrderItems { get; set; } = new();
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
---
+++ Dtos/OrderItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class OrderItemDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int ListedPrice { get; set; }
+        public int Discount { get; set; }
+        public int SellingPrice { get; set; }
+        public int Qty { get; set; }
+    }
+}
