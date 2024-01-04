136c4d930d7fa22a1eeae73a1cc527b030bdbfd6
Nkseervi <nkseervi@gmail.com>
2024-01-04 17:26:09 +0530

    PhonePe Request Dto

---
+++ Dtos/PhonePeRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class PhonePeRequestDto
+    {
+        public string XVerify { get; set; }
+        public string Base64Payload { get; set; }
+    }
+}
