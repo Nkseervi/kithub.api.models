4cfc44c308379a39b36a7c1356c7bd854ff3394d
Nkseervi <nkseervi@gmail.com>
2024-01-04 17:09:04 +0530

    UpdateOrder Dto

---
+++ Dtos/OrderToUpdateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kithub.api.models.Dtos
+{
+    public class OrderToUpdateDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string? Request {  get; set; }
+        public string? Response { get; set; }
+        public string? Callback { get; set; }
+        public string? CheckStatusRequest { get; set; }
+        public string? CheckStatusResponse { get; set; }
+    }
+}
