047a1deca830b5e64b112ba049857007f6996abe
Nkseervi <nkseervi@gmail.com>
2024-01-25 10:43:15 +0530

    OrderItems

--- Dtos/OrderItemDto.cs
+++ Dtos/OrderItemDto.cs
@@ -9,9 +9,9 @@ namespace kithub.api.models.Dtos
     public class OrderItemDto
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
-        public string ProductImageURL { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public string ProductImageURL { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public decimal TotalPrice { get; set; }
         public int Qty { get; set; }
